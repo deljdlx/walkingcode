f13bccec6619c1de19a7c42d8cac176b66b0fd7e
Julien - Biniou <julien.delsescaux@oclock.io>
2020-01-10 00:11:08 +0100

    sync

---
+++ source/autoload.php
@@ -0,0 +1,26 @@
+<?php
+
+
+
+spl_autoload_register(function ($calledClassName) {
+
+    $normalizedClassName = preg_replace('`^\\\\`', '', $calledClassName);
+
+
+    if(strpos($normalizedClassName, 'ElBiniou\WalkingCode') === 0) {
+
+        $relativeClassName = str_replace('ElBiniou\WalkingCode', '', $normalizedClassName);
+        $relativePath = str_replace('\\', '/', $relativeClassName);
+
+
+        $definitionClass = __DIR__.'/class'.$relativePath.'.php';
+        if(is_file($definitionClass)) {
+            include($definitionClass);
+        }
+    }
+
+
+
+});
+
+
